1b8c8240e236d9b8cfa163d95fd5fe25a8ae5f9c
elfi <145452036+elifftorun@users.noreply.github.com>
2024-11-26 12:44:22 +0300

    Add files via upload

---
+++ Ornek3.java
@@ -0,0 +1,23 @@
+package com.example.uygulama1;
+
+import java.util.Scanner;
+
+public class Ornek3 {
+    public static void main(String[] args) {
+        Scanner oku=new Scanner(System.in);
+
+
+
+    }
+    public static int farkHesapla(int s1,int s2){
+
+        if(s1>s2){
+            return s1-s2;
+
+        }
+        else {
+            return s2-s1;
+        }
+
+    }
+}
